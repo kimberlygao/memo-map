5a1559714a5296f67d42a74904d3db22ad50161e
Kimberly Gao <kimberlywgao@gmail.com>
2022-11-02 20:45:32 -0400

    first commit

---
+++ memoMap/memoMapApp.swift
@@ -0,0 +1,30 @@
+//
+//  memoMapApp.swift
+//  memoMap
+//
+//  Created by Kimberly Gao on 11/2/22.
+//
+
+import SwiftUI
+import FirebaseCore
+
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
+
+
+@main
+struct memoMapApp: App {
+  @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
