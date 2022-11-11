0f47a21e538074cef836d26e20da6a06f38aaa02
Kimberly Gao <kimberlywgao@gmail.com>
2022-11-11 01:40:09 -0500

    v1 demo done

--- memoMap/Views/ContentView.swift
+++ memoMap/Views/ContentView.swift
@@ -37,6 +37,7 @@ struct ContentView: View {
                 .foregroundColor(.black)
             }
           }
+          SearchView(mapViewController: mapViewController)
           Spacer()
         }
         VStack {
--- memoMap/Views/SearchView.swift
+++ memoMap/Views/SearchView.swift
@@ -9,23 +9,27 @@ import SwiftUI
 import MapKit
 
 struct SearchView: View {
-    @ObservedObject var mapViewController: MapViewController
-    @State var search: String = ""
-    
-     var body: some View {
-         TextField("Search", text: $search, onEditingChanged: { _ in})
-                   {
-                       print(self.search)
-                       mapViewController.performSearch(search: self.search)
-                   }
-                 .textFieldStyle(RoundedBorderTextFieldStyle())
-                 // Text alignment.
-                 .multilineTextAlignment(.leading)
-                 // Text/placeholder font.
-                 .font(.title.weight(.thin))
-                 .frame(width: 380, height: 60, alignment: .center)
-                 .background(RoundedRectangle(cornerRadius: 10).fill(Color.white))
-     }
+  @ObservedObject var mapViewController: MapViewController
+  @State var search: String = ""
+  
+  var body: some View {
+    TextField("Search", text: $search, onEditingChanged: { _ in})
+    {
+      print(self.search)
+      if self.search == "" {
+        mapViewController.addMapAnnotations()
+      } else {
+        mapViewController.performSearch(search: self.search)
+      }
+    }
+    .textFieldStyle(RoundedBorderTextFieldStyle())
+    // Text alignment.
+    .multilineTextAlignment(.leading)
+    // Text/placeholder font.
+    .font(.title.weight(.thin))
+    .frame(width: 380, height: 60, alignment: .center)
+    .background(RoundedRectangle(cornerRadius: 10).fill(Color.white))
+  }
 }
 
 //struct SearchView_Previews: PreviewProvider {
