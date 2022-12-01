67c7f14f008a18b509f02ed63dbe30c5091bb498
Chloe Chan <chloec@andrew.cmu.edu>
2022-12-01 16:01:21 -0500

    prompt, daily prompt controlelrs + repositories

---
+++ memoMap/Controllers/DailyController.swift
@@ -0,0 +1,27 @@
+//
+//  DailyController.swift
+//  memoMap
+//
+//  Created by Chloe Chan on 12/1/22.
+//
+import Foundation
+import FirebaseFirestore
+import FirebaseFirestoreSwift
+
+class DailyPromptController: ObservableObject {
+  @Published var dailyRepository: DailyPromptRepository = DailyPromptRepository()
+  @Published var memoryController: MemoryController = MemoryController()
+  @Published var dailys: [DailyPrompt] = []
+
+  init() {
+    // get all daily prompt answers
+    self.dailys = dailyRepository.dailys
+  }
+
+  func selectAnswer(user: User, memory: Memory) {
+    let ans = DailyPrompt(id: user.id!, memory: memory.id!)
+    dailyRepository.update(ans)
+  }
+
+
+}
